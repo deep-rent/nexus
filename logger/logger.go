25dfa5c93b39533ec7118832cfd9aa4a67663821
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-23 20:26:08 +0200

    feat: support configuring the log handler

--- logger/logger.go
+++ logger/logger.go
@@ -1,25 +1,94 @@
 package logger
 
 import (
+	"io"
 	"log/slog"
 	"os"
 	"strings"
 )
 
-func New(s string) *slog.Logger {
-	level := slog.LevelInfo // Default level
-	switch strings.ToLower(strings.TrimSpace(s)) {
-	case "debug":
-		level = slog.LevelDebug
-	case "warn":
-		level = slog.LevelWarn
-	case "error":
-		level = slog.LevelError
+type Format uint8
+
+const (
+	FormatJSON Format = iota
+	FormatText
+)
+
+func New(opts ...Option) *slog.Logger {
+	cfg := defaultConfig()
+	for _, opt := range opts {
+		opt(&cfg)
 	}
-	return slog.New(slog.NewJSONHandler(
-		os.Stdout,
-		&slog.HandlerOptions{
-			Level: level,
-		}),
-	)
+
+	w := cfg.Writer
+	o := &slog.HandlerOptions{
+		Level:     cfg.Level,
+		AddSource: cfg.AddSource,
+	}
+
+	var handler slog.Handler
+	switch cfg.Format {
+	case FormatText:
+		handler = slog.NewTextHandler(w, o)
+	default:
+		handler = slog.NewJSONHandler(w, o)
+	}
+
+	return slog.New(handler)
+}
+
+type config struct {
+	Level     slog.Level
+	AddSource bool
+	Format    Format
+	Writer    io.Writer
+}
+
+func defaultConfig() config {
+	return config{
+		Writer: os.Stdout,
+	}
+}
+
+type Option func(*config)
+
+func WithLevel(level string) Option {
+	return func(cfg *config) {
+		cfg.Level = ParseLevel(level)
+	}
+}
+
+func WithFormat(format string) Option {
+	return func(cfg *config) {
+		cfg.Format = ParseFormat(format)
+	}
+}
+
+func WithAddSource(add bool) Option {
+	return func(cfg *config) {
+		cfg.AddSource = add
+	}
+}
+
+func WithWriter(w io.Writer) Option {
+	return func(cfg *config) {
+		if w != nil {
+			cfg.Writer = w
+		}
+	}
+}
+
+func ParseLevel(s string) slog.Level {
+	var level slog.Level
+	_ = level.UnmarshalText([]byte(s))
+	return level
+}
+
+func ParseFormat(s string) Format {
+	switch strings.ToLower(s) {
+	case "text":
+		return FormatText
+	default:
+		return FormatJSON
+	}
 }
