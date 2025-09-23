a9a38b1cc7b8dac99cd51bf8c1dc83309c8d5469
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-23 18:50:49 +0200

    feat: implement linear backoff strategy

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -1,6 +1,19 @@
 package backoff
 
-import "time"
+import (
+	"sync/atomic"
+	"time"
+)
+
+type config struct {
+	minDelay time.Duration
+	maxDelay time.Duration
+}
+
+type linearConfig struct {
+	config
+	step time.Duration
+}
 
 type Strategy interface {
 	Next() time.Duration
@@ -23,3 +36,23 @@ func (b *constant) MinDelay() time.Duration { return b.delay }
 func (b *constant) MaxDelay() time.Duration { return b.delay }
 
 var _ Strategy = (*constant)(nil)
+
+type linear struct {
+	minDelay time.Duration
+	maxDelay time.Duration
+	step     time.Duration
+	attempts atomic.Int64
+}
+
+func (b *linear) Next() time.Duration {
+	n := b.attempts.Add(1) - 1
+	d := b.minDelay + (time.Duration(n) * b.step)
+
+	return min(b.maxDelay, d)
+}
+
+func (b *linear) Done()                   { b.attempts.Store(0) }
+func (b *linear) MinDelay() time.Duration { return b.minDelay }
+func (b *linear) MaxDelay() time.Duration { return b.maxDelay }
+
+var _ Strategy = (*linear)(nil)
