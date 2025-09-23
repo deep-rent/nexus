959ef7881a92bc8edce5e6a1bd394d670252a655
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-23 19:37:03 +0200

    feat: implement jitter decorator

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -1,15 +1,9 @@
 package backoff
 
 import (
+	"math"
 	"sync/atomic"
 	"time"
-)
-
-const (
-	DefaultMinDelay = 1 * time.Second
-	DefaultMaxDelay = 5 * time.Minute
-	DefaultStep     = 1 * time.Second
-	DefaultFactor   = 2.0
 )
 
 type Strategy interface {
@@ -37,29 +31,13 @@ var _ Strategy = (*constant)(nil)
 type linear struct {
 	minDelay time.Duration
 	maxDelay time.Duration
-	step     time.Duration
 	attempts atomic.Int64
 }
 
-func Linear(opts ...Option) Strategy {
-	cfg := defaultConfig()
-	for _, opt := range opts {
-		opt(&cfg)
-	}
-	if cfg.minDelay >= cfg.maxDelay {
-		return Constant(cfg.minDelay)
-	}
-	return &linear{
-		minDelay: cfg.minDelay,
-		maxDelay: cfg.maxDelay,
-		step:     cfg.step,
-	}
-}
-
 func (l *linear) Next() time.Duration {
-	n := l.attempts.Add(1) - 1
-	d := l.minDelay + (time.Duration(n) * l.step)
-	return max(l.minDelay, min(d, l.maxDelay))
+	n := l.attempts.Add(1)
+	d := l.minDelay * time.Duration(n)
+	return max(l.minDelay, min(l.maxDelay, d))
 }
 
 func (l *linear) Done()                   { l.attempts.Store(0) }
@@ -71,70 +49,47 @@ var _ Strategy = (*linear)(nil)
 type exponential struct {
 	minDelay time.Duration
 	maxDelay time.Duration
-	factor   float64
+	growth   float64
 	attempts atomic.Int64
 }
 
-func Exponential(opts ...Option) Strategy {
-	cfg := defaultConfig()
-	for _, opt := range opts {
-		opt(&cfg)
-	}
-	if cfg.minDelay >= cfg.maxDelay {
-		return Constant(cfg.minDelay)
-	}
-	return &exponential{
-		minDelay: cfg.minDelay,
-		maxDelay: cfg.maxDelay,
-		factor:   cfg.factor,
-	}
+func (e *exponential) Next() time.Duration {
+	n := e.attempts.Add(1)
+	d := time.Duration(float64(e.minDelay) * math.Pow(e.growth, float64(n)))
+	return max(e.minDelay, min(e.maxDelay, d))
 }
 
-func (e *exponential) Next() time.Duration     { return 0 }
 func (e *exponential) Done()                   { e.attempts.Store(0) }
 func (e *exponential) MinDelay() time.Duration { return e.minDelay }
 func (e *exponential) MaxDelay() time.Duration { return e.maxDelay }
 
 var _ Strategy = (*exponential)(nil)
 
-type Option func(*config)
-
-type config struct {
-	minDelay time.Duration
-	maxDelay time.Duration
-	step     time.Duration
-	factor   float64
+type Rand interface {
+	Float64() float64
 }
 
-func defaultConfig() config {
-	return config{
-		minDelay: DefaultMinDelay,
-		maxDelay: DefaultMaxDelay,
-		step:     DefaultStep,
-		factor:   DefaultFactor,
-	}
+type jitter struct {
+	strategy Strategy
+	p        float64
+	r        Rand
 }
 
-func WithMinDelay(d time.Duration) Option {
-	return func(c *config) {
-		c.minDelay = max(0, d)
-	}
+func (j *jitter) Next() time.Duration {
+	f := j.r.Float64()
+	return time.Duration(float64(j.strategy.Next()) * (1 - j.p*f))
 }
 
-func WithMaxDelay(d time.Duration) Option {
-	return func(c *config) {
-		c.maxDelay = max(0, d)
-	}
+func (j *jitter) Done() {
+	j.strategy.Done()
 }
 
-func WithStep(d time.Duration) Option {
-	return func(c *config) {
-		c.step = max(0, d)
-	}
+func (j *jitter) MinDelay() time.Duration {
+	return time.Duration(float64(j.strategy.MinDelay()) * j.p)
 }
 
-func WithFactor(f float64) Option {
-	return func(c *config) {
-		c.factor = max(1, f)
-	}
+func (j *jitter) MaxDelay() time.Duration {
+	return j.strategy.MaxDelay()
 }
+
+var _ Strategy = (*jitter)(nil)
