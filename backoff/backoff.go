d4d35b2165b1002c6163c002bd505732e4b322c9
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-23 18:42:51 +0200

    feat: implement constant backoff strategy

--- backoff/backoff.go
+++ backoff/backoff.go
@@ -8,3 +8,18 @@ type Strategy interface {
 	MinDelay() time.Duration
 	MaxDelay() time.Duration
 }
+
+type constant struct {
+	delay time.Duration
+}
+
+func Constant(delay time.Duration) Strategy {
+	return &constant{delay: delay}
+}
+
+func (b *constant) Next() time.Duration     { return b.delay }
+func (b *constant) Done()                   {}
+func (b *constant) MinDelay() time.Duration { return b.delay }
+func (b *constant) MaxDelay() time.Duration { return b.delay }
+
+var _ Strategy = (*constant)(nil)
