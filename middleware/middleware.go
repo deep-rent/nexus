d015a1c28c075fd0884cda57aa022fd7239d9908
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-23 20:47:55 +0200

    feat: filter out nil pipes

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -6,7 +6,9 @@ type Pipe func(http.Handler) http.Handler
 
 func Link(h http.Handler, pipes ...Pipe) http.Handler {
 	for i := len(pipes) - 1; i >= 0; i-- {
-		h = pipes[i](h)
+		if pipe := pipes[i]; pipe != nil {
+			h = pipe(h)
+		}
 	}
 	return h
 }
