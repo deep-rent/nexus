3db37407d7262d404a856fe2d1e80f6ad5727f81
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-23 18:40:12 +0200

    feat: add link function to compose pipes

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -3,3 +3,10 @@ package middleware
 import "net/http"
 
 type Pipe func(http.Handler) http.Handler
+
+func Link(h http.Handler, pipes ...Pipe) http.Handler {
+	for i := len(pipes) - 1; i >= 0; i-- {
+		h = pipes[i](h)
+	}
+	return h
+}
