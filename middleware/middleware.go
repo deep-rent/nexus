d13176abc62c33f9e8f47275d00c19a87fb8261c
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-24 17:05:40 +0200

    refactor: rename link function to chain

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,7 +4,7 @@ import "net/http"
 
 type Pipe func(http.Handler) http.Handler
 
-func Link(h http.Handler, pipes ...Pipe) http.Handler {
+func Chain(h http.Handler, pipes ...Pipe) http.Handler {
 	for i := len(pipes) - 1; i >= 0; i-- {
 		if pipe := pipes[i]; pipe != nil {
 			h = pipe(h)
