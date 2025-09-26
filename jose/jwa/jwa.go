f4120d0d511ad6f3b68f7a2ab83100279381a5a3
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-09-26 18:17:11 +0200

    perf: pool hashers

--- jose/jwa/jwa.go
+++ jose/jwa/jwa.go
@@ -5,109 +5,156 @@ import (
 	"crypto/ecdsa"
 	"crypto/ed25519"
 	"crypto/rsa"
+	"hash"
 	"math/big"
+	"sync"
 
 	"github.com/cloudflare/circl/sign/ed448"
 )
 
 type Scheme[K crypto.PublicKey] interface {
 	Name() string
+	Type() string
 	Verify(key K, msg, sig []byte) bool
 }
 
-type rs struct {
+type rsScheme struct {
 	name string
 	hash crypto.Hash
+	pool sync.Pool
 }
 
-func (a rs) Name() string { return a.name }
+func newRSScheme(name string, hash crypto.Hash) Scheme[*rsa.PublicKey] {
+	return &rsScheme{
+		name: name,
+		hash: hash,
+		pool: sync.Pool{
+			New: func() any {
+				return hash.New()
+			},
+		},
+	}
+}
 
-func (a rs) Verify(key *rsa.PublicKey, msg, sig []byte) bool {
-	h := a.hash.New()
+func (s *rsScheme) Name() string { return s.name }
+func (s *rsScheme) Type() string { return "RSA" }
+
+func (s *rsScheme) Verify(key *rsa.PublicKey, msg, sig []byte) bool {
+	h := s.pool.Get().(hash.Hash)
+	defer func() {
+		h.Reset()
+		s.pool.Put(h)
+	}()
 	h.Write(msg)
 	digest := h.Sum(nil)
-	return rsa.VerifyPKCS1v15(key, a.hash, digest, sig) == nil
+	return rsa.VerifyPKCS1v15(key, s.hash, digest, sig) == nil
 }
 
-var _ Scheme[*rsa.PublicKey] = rs{}
+var RS256 = newRSScheme("RS256", crypto.SHA256)
+var RS384 = newRSScheme("RS384", crypto.SHA384)
+var RS512 = newRSScheme("RS512", crypto.SHA512)
 
-var RS256 = rs{name: "RS256", hash: crypto.SHA256}
-var RS384 = rs{name: "RS384", hash: crypto.SHA384}
-var RS512 = rs{name: "RS512", hash: crypto.SHA512}
-
-type ps struct {
+type psScheme struct {
 	name string
 	hash crypto.Hash
+	pool sync.Pool
 }
 
-func (a ps) Name() string { return a.name }
+func (s *psScheme) Name() string { return s.name }
+func (s *psScheme) Type() string { return "RSA" }
 
-func (a ps) Verify(key *rsa.PublicKey, msg, sig []byte) bool {
-	h := a.hash.New()
+func (s *psScheme) Verify(key *rsa.PublicKey, msg, sig []byte) bool {
+	h := s.pool.Get().(hash.Hash)
+	defer func() {
+		h.Reset()
+		s.pool.Put(h)
+	}()
 	h.Write(msg)
 	digest := h.Sum(nil)
-	return rsa.VerifyPSS(key, a.hash, digest, sig, &rsa.PSSOptions{
+	return rsa.VerifyPSS(key, s.hash, digest, sig, &rsa.PSSOptions{
 		SaltLength: rsa.PSSSaltLengthEqualsHash,
 	}) == nil
 }
 
-var _ Scheme[*rsa.PublicKey] = ps{}
-
-var PS256 = ps{name: "PS256", hash: crypto.SHA256}
-var PS384 = ps{name: "PS384", hash: crypto.SHA384}
-var PS512 = ps{name: "PS512", hash: crypto.SHA512}
-
-type es struct {
-	name string
-	hash crypto.Hash
-	size int
+func newPSScheme(name string, hash crypto.Hash) Scheme[*rsa.PublicKey] {
+	return &psScheme{
+		name: name,
+		hash: hash,
+		pool: sync.Pool{
+			New: func() any {
+				return hash.New()
+			},
+		},
+	}
 }
 
-func (a es) Name() string { return a.name }
+var PS256 = newPSScheme("PS256", crypto.SHA256)
+var PS384 = newPSScheme("PS384", crypto.SHA384)
+var PS512 = newPSScheme("PS512", crypto.SHA512)
 
-func (a es) Verify(key *ecdsa.PublicKey, msg, sig []byte) bool {
-	if len(sig) != 2*a.size {
+type esScheme struct {
+	name string
+	pool sync.Pool
+}
+
+func (s *esScheme) Name() string { return s.name }
+func (s *esScheme) Type() string { return "EC" }
+
+func (a *esScheme) Verify(key *ecdsa.PublicKey, msg, sig []byte) bool {
+	n := (key.Curve.Params().BitSize + 7) / 8
+
+	if len(sig) != 2*n {
 		return false
 	}
 
-	r := new(big.Int).SetBytes(sig[:a.size])
-	s := new(big.Int).SetBytes(sig[a.size:])
+	r := new(big.Int).SetBytes(sig[:n])
+	s := new(big.Int).SetBytes(sig[n:])
 
-	h := a.hash.New()
+	h := a.pool.Get().(hash.Hash)
+	defer func() {
+		h.Reset()
+		a.pool.Put(h)
+	}()
 	h.Write(msg)
 	digest := h.Sum(nil)
 	return ecdsa.Verify(key, digest, r, s)
 }
 
-var _ Scheme[*ecdsa.PublicKey] = es{}
+func newESScheme(name string, hash crypto.Hash) Scheme[*ecdsa.PublicKey] {
+	return &esScheme{
+		name: name,
+		pool: sync.Pool{
+			New: func() any {
+				return hash.New()
+			},
+		}}
+}
 
-var ES256 = es{name: "ES256", hash: crypto.SHA256, size: 32}
-var ES384 = es{name: "ES384", hash: crypto.SHA384, size: 48}
-var ES512 = es{name: "ES512", hash: crypto.SHA512, size: 66}
+var ES256 = newESScheme("ES256", crypto.SHA256)
+var ES384 = newESScheme("ES384", crypto.SHA384)
+var ES512 = newESScheme("ES512", crypto.SHA512)
 
-type ed1 struct{}
+type ed25519Scheme struct{}
 
-func (a ed1) Name() string { return "EdDSA" }
+func (s ed25519Scheme) Name() string { return "EdDSA" }
+func (s ed25519Scheme) Type() string { return "OKP" }
 
-func (a ed1) Verify(key ed25519.PublicKey, msg, sig []byte) bool {
+func (s ed25519Scheme) Verify(key ed25519.PublicKey, msg, sig []byte) bool {
 	return ed25519.Verify(key, msg, sig)
 }
 
-var _ Scheme[ed25519.PublicKey] = ed1{}
+var Ed25519 Scheme[ed25519.PublicKey] = ed25519Scheme{}
 
-var Ed25519 = ed1{}
+type ed448Scheme struct{}
 
-type ed2 struct{}
+func (s ed448Scheme) Name() string { return "EdDSA" }
+func (s ed448Scheme) Type() string { return "OKP" }
 
-func (a ed2) Name() string { return "EdDSA" }
-
-func (a ed2) Verify(key ed448.PublicKey, msg, sig []byte) bool {
+func (s ed448Scheme) Verify(key ed448.PublicKey, msg, sig []byte) bool {
 	return ed448.Verify(key, msg, sig, "")
 }
 
-var _ Scheme[ed448.PublicKey] = ed2{}
-
-var Ed448 = ed2{}
+var Ed448 Scheme[ed448.PublicKey] = ed448Scheme{}
 
 type Verifier interface {
 	Algorithm() string
