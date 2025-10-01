7a5f75c1bbd232fc5c22c08f2565004d64854d79
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-01 16:11:50 +0200

    feat: add map utility functions

---
+++ util/util.go
@@ -0,0 +1,17 @@
+package util
+
+func Keys[K comparable, V any](m map[K]V) []K {
+	keys := make([]K, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+func Vals[K comparable, V any](m map[K]V) []V {
+	vals := make([]V, 0, len(m))
+	for _, v := range m {
+		vals = append(vals, v)
+	}
+	return vals
+}
