f539b1b554dac821f49a2aa88d43bbf114c10cca
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-01 16:13:38 +0200

    docs: add missing documentation comments

--- util/util.go
+++ util/util.go
@@ -18,6 +18,7 @@ func Conv[T any](v any) (T, error) {
 	return vt, nil
 }
 
+// Keys returns the keys of the given map as a slice.
 func Keys[K comparable, V any](m map[K]V) []K {
 	keys := make([]K, 0, len(m))
 	for k := range m {
@@ -26,6 +27,7 @@ func Keys[K comparable, V any](m map[K]V) []K {
 	return keys
 }
 
+// Vals returns the values of the given map as a slice.
 func Vals[K comparable, V any](m map[K]V) []V {
 	vals := make([]V, 0, len(m))
 	for _, v := range m {
