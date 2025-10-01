3bca7fc7a7d2acacda27c7ea15617e946b71ac29
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-01 16:12:23 +0200

    feat: add zero value utility

--- util/util.go
+++ util/util.go
@@ -1,5 +1,11 @@
 package util
 
+// Zero returns the zero value of type T.
+func Zero[T any]() T {
+	var zero T
+	return zero
+}
+
 func Keys[K comparable, V any](m map[K]V) []K {
 	keys := make([]K, 0, len(m))
 	for k := range m {
