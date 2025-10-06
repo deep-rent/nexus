a2137c49cace2df9e3c0316439b9d7d650a925b2
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-06 19:35:01 +0200

    feat: add recover middleware for panics

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,6 +1,10 @@
 package middleware
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"runtime/debug"
+)
 
 type Pipe func(http.Handler) http.Handler
 
@@ -11,4 +15,30 @@ func Chain(h http.Handler, pipes ...Pipe) http.Handler {
 		}
 	}
 	return h
+}
+
+// Recover produces a middleware Pipe that catches panics in downstream
+// handlers. It uses the provided logger to report the exception with a stack
+// trace and returns an empty response with status code 500 to the client. The
+// log entry also pinpoints the request method and URL that caused the panic.
+func Recover(logger *slog.Logger) Pipe {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					method, url := r.Method, r.URL.String()
+					logger.Error(
+						"panic caught by middleware",
+						"method", method,
+						"url", url,
+						"error", err,
+						"stack", string(debug.Stack()),
+					)
+					w.WriteHeader(http.StatusInternalServerError)
+				}
+			}()
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
