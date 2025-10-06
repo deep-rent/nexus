2638e66f14fbeec6c8dcbdac21fd140defc45239
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-06 19:23:15 +0200

    feat: add shutdown utility

--- signal/signal.go
+++ signal/signal.go
@@ -1,2 +1,17 @@
 // Package signal provides utilities for handling operating system signals.
 package signal
+
+import (
+	"os"
+	"syscall"
+)
+
+// Shutdown lists signals that should trigger a graceful shutdown.
+func Shutdown() []os.Signal {
+	return []os.Signal{
+		syscall.SIGTERM,
+		syscall.SIGINT,
+		syscall.SIGQUIT,
+		syscall.SIGKILL,
+	}
+}
