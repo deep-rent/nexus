f9d469bf28006215f4839b68851a535d087a928b
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-07 19:50:15 +0200

    docs: polish documentation comments

--- app/app.go
+++ app/app.go
@@ -7,14 +7,20 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
-
-	"github.com/deep-rent/nexus/signal"
 )
 
 // DefaultShutdownTimeout is the default duration to wait for the application to
 // gracefully shut down after receiving a termination signal.
 const DefaultShutdownTimeout = 10 * time.Second
+
+// Runnable defines a function that can be executed by the application runner.
+// It receives a context that is canceled when a shutdown signal is received.
+// The function should perform its cleanup and return when the context is done.
+type Runnable func(ctx context.Context) error
 
 type config struct {
 	logger  *slog.Logger
@@ -24,6 +30,8 @@ type config struct {
 // Option is a function that configures the application runner.
 type Option func(*config)
 
+// WithLogger provides a custom logger for the application runner. If not set,
+// the runner defaults to slog.Default(). A nil value will be ignored.
 func WithLogger(logger *slog.Logger) Option {
 	return func(opts *config) {
 		if logger != nil {
@@ -34,7 +42,9 @@ func WithLogger(logger *slog.Logger) Option {
 
 // WithShutdownTimeout sets a custom timeout for the graceful shutdown process.
 // If the application logic takes longer than this duration to return after a
-// shutdown signal is received, the runner will exit with an error.
+// shutdown signal is received, the runner will exit with an error. A
+// non-positive duration will be ignored, and the DefaultShutdownTimeout is
+// used instead.
 func WithShutdownTimeout(d time.Duration) Option {
 	return func(opts *config) {
 		if d > 0 {
@@ -43,8 +53,16 @@ func WithShutdownTimeout(d time.Duration) Option {
 	}
 }
 
-// Run executes the main application logic and handles its lifecycle.
-func Run(main func(ctx context.Context) error, opts ...Option) error {
+// Run provides a managed execution environment for a Runnable.
+// It launches the Runnable in a separate goroutine and blocks until it
+// either completes on its own or an OS interrupt signal is caught.
+//
+// Upon receiving a signal, it cancels the context passed to the Runnable
+// and waits for the specified shutdown timeout. The Runnable is expected
+// to honor the context cancellation and perform any necessary cleanup before
+// returning. Run returns any error from the Runnable itself, or an error
+// if the shutdown process times out.
+func Run(fn Runnable, opts ...Option) error {
 	cfg := config{
 		logger:  slog.Default(),
 		timeout: DefaultShutdownTimeout,
@@ -55,20 +73,24 @@ func Run(main func(ctx context.Context) error, opts ...Option) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	sigCh := signal.Shutdown()
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+
 	errCh := make(chan error, 1)
-	go func() { errCh <- main(ctx) }()
+	go func() { errCh <- fn(ctx) }()
 
 	cfg.logger.Info("Application started")
 
 	select {
+	// Listen for application errors.
 	case err := <-errCh:
 		if err != nil {
-			return fmt.Errorf("application error: %w", err)
+			return fmt.Errorf("encountered an application error: %w", err)
 		}
 		cfg.logger.Info("Application stopped")
 		return nil
 
+		// Listen for gracious termination signals.
 	case sig := <-sigCh:
 		cfg.logger.Info(
 			"Initiating graceful shutdown",
@@ -79,7 +101,7 @@ func Run(main func(ctx context.Context) error, opts ...Option) error {
 		select {
 		case err := <-errCh:
 			if err != nil {
-				return fmt.Errorf("error during shutdown: %w", err)
+				return fmt.Errorf("error occurred during shutdown: %w", err)
 			}
 			cfg.logger.Info("Shutdown completed")
 			return nil
