64b4c2e301ef50a5026bcc83b4c4e8a8d4d02a88
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-10-10 21:52:03 +0200

    refactor: move snake case transform into separate package

--- env/env.go
+++ env/env.go
@@ -110,6 +110,8 @@ import (
 	"strings"
 	"time"
 	"unicode"
+
+	"github.com/deep-rent/nexus/snake"
 )
 
 // Lookup is a function that retrieves the value of an environment variable.
@@ -308,7 +310,7 @@ func process(rv reflect.Value, prefix string, lookup Lookup) error {
 
 		key := opts.Name
 		if key == "" {
-			key = snake(ft.Name)
+			key = snake.ToUpper(ft.Name)
 		}
 
 		// Check for true embedded structs.
@@ -724,30 +726,4 @@ func unquote(s string) string {
 	}
 	// Return the original string if no matching quotes are found.
 	return s
-}
-
-// snake converts a camelCase string to an uppercase SNAKE_CASE string.
-// For example: "myVariable" -> "MY_VARIABLE", "APIService" -> "API_SERVICE".
-func snake(s string) string {
-	var b strings.Builder
-	b.Grow(len(s) + 5)
-	runes := []rune(s)
-	for i, r := range runes {
-		// Insert an underscore before a capital letter or digit.
-		if i != 0 {
-			q := runes[i-1]
-			if (unicode.IsLower(q) &&
-				// Case 1: Lowercase to uppercase/digit transition ("myVar", "myVar1").
-				(unicode.IsUpper(r) || unicode.IsDigit(r))) ||
-				(unicode.IsUpper(q) &&
-					// Case 2: Acronym to new word transition ("MYVar").
-					unicode.IsUpper(r) &&
-					i+1 < len(runes) &&
-					unicode.IsLower(runes[i+1])) {
-				b.WriteRune('_')
-			}
-		}
-		b.WriteRune(unicode.ToUpper(r))
-	}
-	return b.String()
 }
---
+++ snake/snake.go
@@ -0,0 +1,38 @@
+package snake
+
+import (
+	"strings"
+	"unicode"
+)
+
+// ToUpper converts a camelCase string to an uppercase SNAKE_CASE string.
+// For example: "myVariable" -> "MY_VARIABLE", "APIService" -> "API_SERVICE".
+func ToUpper(s string) string { return transform(s, unicode.ToUpper) }
+
+// ToLower converts a camelCase string to a lowercase snake_case string.
+// For example: "myVariable" -> "my_variable", "APIService" -> "api_service".
+func ToLower(s string) string { return transform(s, unicode.ToLower) }
+
+func transform(s string, toCase func(rune) rune) string {
+	var b strings.Builder
+	b.Grow(len(s) + 5)
+	runes := []rune(s)
+	for i, r := range runes {
+		// Insert an underscore before a capital letter or digit.
+		if i != 0 {
+			q := runes[i-1]
+			if (unicode.IsLower(q) &&
+				// Case 1: Lowercase to uppercase/digit transition ("myVar", "myVar1").
+				(unicode.IsUpper(r) || unicode.IsDigit(r))) ||
+				(unicode.IsUpper(q) &&
+					// Case 2: Acronym to new word transition ("MYVar").
+					unicode.IsUpper(r) &&
+					i+1 < len(runes) &&
+					unicode.IsLower(runes[i+1])) {
+				b.WriteRune('_')
+			}
+		}
+		b.WriteRune(toCase(r))
+	}
+	return b.String()
+}
