c3b356ad3e128bee3a031c9d1fb5b7e92bbfe03a
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2025-11-26 23:13:05 +0100

    test: check consistency of sign/verify methods

--- jose/jwa/jwa_test.go
+++ jose/jwa/jwa_test.go
@@ -1,3 +1,86 @@
 package jwa_test
 
-// This package is already fully covered by tests in jwk/jwk_test.go.
+import (
+	"crypto/ecdsa"
+	"crypto/ed25519"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"testing"
+
+	"github.com/cloudflare/circl/sign/ed448"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/deep-rent/nexus/jose/jwa"
+)
+
+var msg = []byte("payload")
+
+func TestRSA(t *testing.T) {
+	k, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	tests := []struct {
+		n string
+		a jwa.Algorithm[*rsa.PublicKey]
+	}{
+		{"RS256", jwa.RS256},
+		{"RS384", jwa.RS384},
+		{"RS512", jwa.RS512},
+		{"PS256", jwa.PS256},
+		{"PS384", jwa.PS384},
+		{"PS512", jwa.PS512},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.n, func(t *testing.T) {
+			sig, err := tc.a.Sign(k, msg)
+			require.NoError(t, err)
+			assert.True(t, tc.a.Verify(&k.PublicKey, msg, sig))
+		})
+	}
+}
+
+func TestECDSA(t *testing.T) {
+	tests := []struct {
+		n string
+		a jwa.Algorithm[*ecdsa.PublicKey]
+		c elliptic.Curve
+	}{
+		{"ES256", jwa.ES256, elliptic.P256()},
+		{"ES384", jwa.ES384, elliptic.P384()},
+		{"ES512", jwa.ES512, elliptic.P521()},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.n, func(t *testing.T) {
+			k, err := ecdsa.GenerateKey(tc.c, rand.Reader)
+			require.NoError(t, err)
+
+			sig, err := tc.a.Sign(k, msg)
+			require.NoError(t, err)
+			assert.True(t, tc.a.Verify(&k.PublicKey, msg, sig))
+		})
+	}
+}
+
+func TestEdDSA(t *testing.T) {
+	t.Run("Ed25519", func(t *testing.T) {
+		pub, prv, err := ed25519.GenerateKey(rand.Reader)
+		require.NoError(t, err)
+
+		sig, err := jwa.EdDSA.Sign(prv, msg)
+		require.NoError(t, err)
+		assert.True(t, jwa.EdDSA.Verify(pub, msg, sig))
+	})
+
+	t.Run("Ed448", func(t *testing.T) {
+		pub, prv, err := ed448.GenerateKey(rand.Reader)
+		require.NoError(t, err)
+
+		sig, err := jwa.EdDSA.Sign(prv, msg)
+		require.NoError(t, err)
+		assert.True(t, jwa.EdDSA.Verify(pub, msg, sig))
+	})
+}
