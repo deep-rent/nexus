f5daf8ccbefc19b3375bff3e406b3f7b03dd3731
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-03-25 01:46:26 +0100

    test: verify checksum calculation

--- internal/checksum/checksum_test.go
+++ internal/checksum/checksum_test.go
@@ -13,3 +13,37 @@
 // limitations under the License.
 
 package checksum_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/deep-rent/nexus/internal/checksum"
+)
+
+func TestBytes(t *testing.T) {
+	tests := []struct {
+		name string
+		in   []byte
+		want string
+	}{
+		{
+			name: "empty input",
+			in:   []byte(""),
+			want: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+		},
+		{
+			name: "hello world",
+			in:   []byte("hello world"),
+			want: "b94d27b9934d3e08a52e52d7da7dabfac484efe37a5380ee9088f7ace2efcde9",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got := checksum.Bytes(tc.in)
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
