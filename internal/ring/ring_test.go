179c3a69cb0ded1c500d174ed7ce4ff1bbefff8b
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-03-31 17:10:05 +0200

    test: add unit tests for ring package

--- internal/ring/ring_test.go
+++ internal/ring/ring_test.go
@@ -13,3 +13,91 @@
 // limitations under the License.
 
 package ring_test
+
+import (
+	"testing"
+
+	"github.com/deep-rent/nexus/internal/ring"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	tcs := []struct {
+		name string
+		size int
+		exp  int
+	}{
+		{"neg", -1, 2},
+		{"zero", 0, 2},
+		{"one", 1, 2},
+		{"two", 2, 2},
+		{"three", 3, 4},
+		{"four", 4, 4},
+		{"five", 5, 8},
+		{"large", 1000, 1024},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			b := ring.New[int](tc.size, ring.DropNewest)
+			c := 0
+			for b.Push(c) {
+				c++
+			}
+			assert.Equal(t, tc.exp, c)
+		})
+	}
+}
+
+func TestPushPop(t *testing.T) {
+	b := ring.New[string](4, ring.Block)
+
+	_, ok := b.Pop()
+	assert.False(t, ok)
+
+	assert.True(t, b.Push("a"))
+	assert.True(t, b.Push("b"))
+
+	v, ok := b.Pop()
+	require.True(t, ok)
+	assert.Equal(t, "a", v)
+
+	v, ok = b.Pop()
+	require.True(t, ok)
+	assert.Equal(t, "b", v)
+
+	_, ok = b.Pop()
+	assert.False(t, ok)
+}
+
+func TestDropNewest(t *testing.T) {
+	b := ring.New[int](4, ring.DropNewest)
+
+	for i := 0; i < 4; i++ {
+		assert.True(t, b.Push(i))
+	}
+
+	assert.False(t, b.Push(4))
+
+	v, ok := b.Pop()
+	require.True(t, ok)
+	assert.Equal(t, 0, v)
+}
+
+func TestDropOldest(t *testing.T) {
+	b := ring.New[int](4, ring.DropOldest)
+
+	for i := 0; i < 6; i++ {
+		b.Push(i)
+	}
+
+	v, ok := b.Pop()
+	require.True(t, ok)
+	assert.Equal(t, 2, v)
+
+	v, ok = b.Pop()
+	require.True(t, ok)
+	assert.Equal(t, 3, v)
+}
