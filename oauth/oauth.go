1d09a295ac969dd978f70d3c6ba9fbe0a23600f4
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-05-11 11:35:57 +0200

    docs: remove device code grant from usage example

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -47,7 +47,6 @@
 //	p.Register(oauth.AuthCodeGrant())
 //	p.Register(oauth.ClientCredentialsGrant())
 //	p.Register(oauth.RefreshTokenGrant())
-//	p.Register(oauth.DeviceCodeGrant())
 //
 //	// Mount the endpoints onto a router.
 //	r := router.New()
