a1d735ac4f4568d56d98eb1f96d3997a300dc124
Tarik Hrnjica <tarik.hrnjica@deep.rent>
2026-06-13 15:03:03 +0200

    chore: add copyright header

--- pkce/pkce_test.go
+++ pkce/pkce_test.go
@@ -1,3 +1,17 @@
+// Copyright (c) 2025-present deep.rent GmbH (https://deep.rent)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package pkce_test
 
 import (
@@ -137,7 +151,9 @@ func TestVerify(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := pkce.Verify(tt.verifier, tt.challenge, tt.method); got != tt.want {
+			if got := pkce.Verify(
+				tt.verifier, tt.challenge, tt.method,
+			); got != tt.want {
 				t.Errorf("Verify() = %v, want %v", got, tt.want)
 			}
 		})
