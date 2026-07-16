2f3942804835412e0daa9ac08c5c40eefad26328
tarikhrnjica <tarik.hrnjica@gmail.com>
2026-07-16 13:54:52 +0200

    refactor(diff): pass graph error to panic

--- diff/registry.go
+++ diff/registry.go
@@ -218,7 +218,7 @@ func (r *Registry[Tx]) order() []string {
 
 	sorted, err := g.Sort()
 	if err != nil {
-		panic("entity type dependencies contain a cycle")
+		panic(err)
 	}
 
 	r.sorted = sorted
